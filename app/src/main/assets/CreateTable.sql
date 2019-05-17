ce52f7e696c38e43278065200ef990bf05dd5354
张禺 <zhangyu@sunmi.com>
2019-05-17 08:42:43 +0800

    每次都重建表

--- app/src/main/assets/CreateTable.sql
+++ app/src/main/assets/CreateTable.sql
@@ -1,3 +1,7 @@
+drop table cs_table;
+drop table fw_check_table;
+drop table hzp_table;
+drop table jxs_table;
 CREATE TABLE IF NOT EXISTS `cs_table`(`csId` TEXT  PRIMARY KEY,`csName` TEXT,`adress` TEXT,`telephone` TEXT,`chDate` DATETIME,`hzpId` TEXT);
 CREATE TABLE IF NOT EXISTS `fw_check_table`(`No` INTEGER PRIMARY KEY AUTOINCREMENT,`hzpId` TEXT,`bqId` TEXT,`check` INTEGER);
 CREATE TABLE IF NOT EXISTS `hzp_table`(`hzpId` TEXT PRIMARY KEY,`hzpName` TEXT,`scDate` DATETIME,`bzq` TEXT);
